d554856e7c1620af14c7f9ab09c6d21ea63caf33
Lorena Quezada <lp.quezadan@uea.edu.ec>
2026-08-16 20:03:58 -0500

    Tarea de la semana 09 creada

---
+++ Semana 09/Tarea Semana 09.py
@@ -0,0 +1,11 @@
+# Tarea Semana 9 - Arreglo simple
+# Creamos un arreglo con los valores solicitados
+arreglo = [10, 20, 30, 40, 50]
+
+# Mostrar el valor en la posición 1
+print("Valor en la posición 1:", arreglo[1])
+
+# Recorrer todo el arreglo desde la primera posición
+print("Elementos del arreglo:")
+for i in range(len(arreglo)):
+    print(arreglo[i])
